e6fa246654a5c4aa74292b81b5e09d7ed07d15b0
Sneha kamgond <you@example.com>
2026-03-04 20:18:03 +0530

    Initial Spring Boot project setup with database configuration

---
+++ src/main/java/com/spring/smartexpense/SmartExpenseAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.spring.smartexpense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartExpenseAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartExpenseAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/smartexpense/SmartExpenseAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.smartexpense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartExpenseAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

6a0712a60517e6696b2922381cc6c30a25f03832
Sneha kamgond <you@example.com>
2026-03-05 11:40:20 +0530

    Added Budget Entity

---
+++ src/main/java/com/spring/smartexpense/entity/Budget.java
@@ -0,0 +1,27 @@
+package com.spring.smartexpense.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "budgets")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Budget {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Double amount;
+
+    private Integer month;
+
+    private Integer year;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}

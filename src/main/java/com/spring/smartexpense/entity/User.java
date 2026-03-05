b0b8d36500c40a888264c3ae0174c57beac69672
Sneha kamgond <you@example.com>
2026-03-05 11:17:48 +0530

    Added User entity

---
+++ src/main/java/com/spring/smartexpense/entity/User.java
@@ -0,0 +1,30 @@
+package com.spring.smartexpense.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="users")
+@Data
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType
+    .IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+
+}

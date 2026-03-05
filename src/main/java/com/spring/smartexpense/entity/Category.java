01747603155b3cd6fc99a6d3d02edb3c57e55e52
Sneha kamgond <you@example.com>
2026-03-05 11:37:44 +0530

    Added Category Entity

---
+++ src/main/java/com/spring/smartexpense/entity/Category.java
@@ -0,0 +1,19 @@
+package com.spring.smartexpense.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+}

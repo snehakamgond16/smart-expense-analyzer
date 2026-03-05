45dfe6f139270e3a8279dc36f7b00c08f3e65daa
Sneha kamgond <you@example.com>
2026-03-05 11:33:39 +0530

    Added Expense Entity

---
+++ src/main/java/com/spring/smartexpense/entity/Expense.java
@@ -0,0 +1,30 @@
+package com.spring.smartexpense.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "expenses")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Expense {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Double amount;
+
+    private String description;
+
+    private LocalDate date;
+
+    private String category;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
